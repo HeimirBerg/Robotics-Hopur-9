e69b5ae85475bc33ad439e184d94c5f5e9917d3e
HeimirBerg <heimir24@ru.is>
2026-04-28 14:42:07 +0000

    skjol

---
+++ hreyfing.py
@@ -0,0 +1,14 @@
+def fara_afram():
+    pass
+
+def fara_aftur():
+    pass
+
+def beyja(haegri,vinstri):
+    pass
+
+def stoppa():
+    pass
+
+
+    
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 from skynjun import *
 from hreyfing import *
+
+
