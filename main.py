d6d12925ca6068f2c4b723326096690af583594c
HeimirBerg <heimir24@ru.is>
2026-04-28 15:03:12 +0000

    uppfærsla

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from skynjun import *
 from hreyfing import *
 
-
+val = input("Veldu Átt(u,n,h,v)")
+if val == u:
+    print(fara_afram())
+elif val == n:
+    print(fara_aftur())
+elif val == h:
+    print(beyja("Hægri"))
+elif val == v:
+    print(beyja("Vinstri"))
+else:
+    print("villa")
