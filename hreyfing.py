1154f2fb88c0737e477b06f40de1bfb1a58b6b86
HeimirBerg <heimir24@ru.is>
2026-04-28 15:17:58 +0000

    uppfærsla

--- hreyfing.py
+++ hreyfing.py
@@ -25,7 +25,7 @@ def fara_aftur():
 def beyja(att):
     if att == "Hægri":
         m1 = int(abs(255))
-        m2 = int(abs(0))
+        m2 = int(abs(-100))
         m1_sign  = 0 if m1 >= 0 else 1
         m2_sign  = 0 if m2 >= 0 else 1
         data = [m1, m1_sign, m2, m2_sign]
@@ -33,7 +33,7 @@ def beyja(att):
         return f"Sent speeds → Motor1: {m1}, Motor2: {m2}"
 
     elif att == "Vinstri":
-        m1 = int(abs(0))
+        m1 = int(abs(100))
         m2 = int(abs(-255))
         m1_sign  = 0 if m1 >= 0 else 1
         m2_sign  = 0 if m2 >= 0 else 1
