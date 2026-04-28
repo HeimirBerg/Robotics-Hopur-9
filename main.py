f0517c32855a02f93136c446e4015adec1ca12d6
HeimirBerg <heimir24@ru.is>
2026-04-28 15:08:05 +0000

    uppfærsla

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from skynjun import *
 from hreyfing import *
 while True:
-    val = input("Veldu Átt(u,n,h,v eða 0 til að hætta: )")
-    if val == u:
+    val = input("Veldu Átt(w,a,s,d eða 0 til að hætta): ")
+    if val == "w":
         print(fara_afram())
-    elif val == n:
+    elif val == "s":
         print(fara_aftur())
-    elif val == h:
+    elif val == "d":
         print(beyja("Hægri"))
-    elif val == v:
+    elif val == "a":
         print(beyja("Vinstri"))
-    elif val == 0:
+    elif val == "0":
         break
     else:
         print("villa")
