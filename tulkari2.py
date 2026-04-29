f11b78b5938ba5ecfda577f596b2980daae52ca7
viktornoes <viktoro24@ru.is>
2026-04-29 17:15:11 +0000

    Prófa hlutfall

--- tulkari2.py
+++ tulkari2.py
@@ -23,9 +23,24 @@ def sense():
         current_value1 = 255
     if current_value2 == 0:
         current_value2 = 255
+    
+    if current_value1 == current_value2:
+        hlutf = 1
+        lengri = "jafnt"
+    elif current_value1 > current_value2:
+        hlutf = current_value1/current_value2
+        lengri = "1 er stærri"
+    elif current_value2 > current_value1:
+        hlutf = current_value2/current_value1
+        lengri = "2 er stærri"
 
-    print(f"Skynjari 1: {current_value1} cm    Skynjari 2: {current_value2} cm")
+    print(f"Skynjari 1: {current_value1} cm    Skynjari 2: {current_value2} cm      Hlutfall: {hlutf} {lengri}")
     return current_value1, current_value2
+
+def hlutfall(a,b):
+    v = a/255
+    h = b/255
+    
 
 while True:
     sjon = sense()
