7f09dc0a9e5983b3d5941268841834848eb093ae
Gunnar237 <gunnarg25@ru.is>
2026-04-29 12:53:32 +0000

    Bætt við vali á hraða

--- hreyfing.py
+++ hreyfing.py
@@ -13,23 +13,42 @@ def senda(m1,m2):
     bus.write_i2c_block_data(I2C_ADDRESS, 0x00, data)
     return f"Sent speeds → Motor1: {m1}, Motor2: {m2}"
 
-def fara_afram():
-    return senda(225,-225)
+def fara_afram(hradi):
+    return senda(hradi,-hradi)
     
-def fara_aftur():
-    return senda(-225,225)
+def fara_aftur(hradi):
+    return senda(-hradi,hradi)
 
-def beyja(att):
+def beyja(att,hradi):
     if att == "Hægri":
-        return senda(225,-100)
+        return senda(hradi,-(hradi/2.25))
 
     elif att == "Vinstri":
-        return senda(100,-225)
+        return senda((hradi/2.25),-hradi)
     else: 
         return "Villa! skrifaðu annað hvort \"Hægri\" eða \"Vinstri\""
 
 def stoppa():
     return senda(0,0)
+
+def velja_hrada():
+    gildi = -1
+    while not (0 < gildi <= 255):
+        try:
+            gildi = int(input("Veldu hraða [1-255]: "))
+
+            if not (0 < gildi <= 255):
+                print("Villa! Sláðu inn heiltölu á bilinu [1-255].")
+                continue
+            else:
+                hradi = gildi
+                break
+
+        except ValueError:
+            print("Villa! Sláðu inn heiltölu á bilinu [1-255].")
+            continue
+
+    return hradi
 
 
 
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from skynjun import *
 from hreyfing import *
+
+hradi = velja_hrada()
+
 i = 1
-while i == 1:
+while True:
     val = input("Veldu Átt(w,a,s,d eða 0 til að hætta): ")
     if val == "w":
-        print(fara_afram())
+        print(fara_afram(hradi))
     elif val == "s":
-        print(fara_aftur())
+        print(fara_aftur(hradi))
     elif val == "d":
-        print(beyja("Hægri"))
+        print(beyja("Hægri",hradi))
     elif val == "a":
-        print(beyja("Vinstri"))
+        print(beyja("Vinstri",hradi))
     elif val == "0":
         print(stoppa())
         break
+    elif val == "r":
+        velja_hrada()
     else:
         print("villa")
