3c46accbffe65eb15f2985a193728819aa4332f6
HeimirBerg <heimir24@ru.is>
2026-04-30 12:43:08 +0000

    w

--- servo.py
+++ servo.py
@@ -3,8 +3,11 @@ from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=8)
 
-kit.servo[0].angle = 45
+kit.servo[0].angle = 90
 kit.servo[1].angle = 90
+time.sleep(1)
+kit.servo[0].angle = 80
+kit.servo[1].angle = 80
 kit.servo[0].angle = None
 kit.servo[1].angle = None
 print("keyrt")
