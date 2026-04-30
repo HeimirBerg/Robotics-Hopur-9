3a30cc22ed413d597413c803ddf1bfd7b79ab213
Paczio24 <patryk24@ru.is>
2026-04-30 12:09:03 +0000

    servo v1

--- servo.py
+++ servo.py
@@ -6,9 +6,9 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=8)
 
 while True:
-print("Moving to 180")
-kit.servo[0].angle = 180
-time.sleep(1)
-print("Moving to 0")
-kit.servo[0].angle = 0
-time.sleep(1)
+    print("Moving to 180")
+    kit.servo[0].angle = 180
+    time.sleep(1)
+    print("Moving to 0")
+    kit.servo[0].angle = 0
+    time.sleep(1)
