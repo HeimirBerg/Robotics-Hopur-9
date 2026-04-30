71d0a3681aa5dc6d919319455d1770a741fbed56
HeimirBerg <heimir24@ru.is>
2026-04-30 12:49:38 +0000

    w

--- servo.py
+++ servo.py
@@ -7,8 +7,11 @@ kit.servo[0].angle = 90
 kit.servo[1].angle = 90
 time.sleep(1)
 kit.servo[0].angle = 80
-kit.servo[1].angle = 80
-time.sleep(1)  
+kit.servo[1].angle = 100
+time.sleep(1) 
+kit.servo[0].angle = 0
+kit.servo[1].angle = 180
+time.sleep(1) 
 kit.servo[0].angle = None
 kit.servo[1].angle = None
 print("keyrt")
