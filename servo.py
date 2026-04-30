07589664061df416856c4a34d4e834f124a33c82
HeimirBerg <heimir24@ru.is>
2026-04-30 12:25:27 +0000

    l

--- servo.py
+++ servo.py
@@ -1,12 +1,6 @@
 import time
-
 from adafruit_servokit import ServoKit
 
-# Initialize for 8 channels
 kit = ServoKit(channels=8)
 
-
-print("Moving to 90")
 kit.servo[0].angle = 90
-time.sleep(1)
-
