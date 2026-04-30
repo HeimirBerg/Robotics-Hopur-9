e736e446094947b7657ea4ff81e9448233fca8ec
Paczio24 <patryk24@ru.is>
2026-04-30 12:12:28 +0000

    Servo test 1

--- servo.py
+++ servo.py
@@ -5,10 +5,8 @@ from adafruit_servokit import ServoKit
 # Initialize for 8 channels
 kit = ServoKit(channels=8)
 
-while True:
-    print("Moving to 180")
-    kit.servo[0].angle = 180
-    time.sleep(1)
-    print("Moving to 0")
-    kit.servo[0].angle = 0
-    time.sleep(1)
+
+print("Moving to 90")
+kit.servo[0].angle = 90
+time.sleep(1)
+
