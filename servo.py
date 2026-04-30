8567ba638477ce41b212413652ecaa2e3dcfa1c8
HeimirBerg <heimir24@ru.is>
2026-04-30 14:11:17 +0000

    i

--- servo.py
+++ servo.py
@@ -9,8 +9,8 @@ time.sleep(1)
 kit.servo[0].angle = 180
 kit.servo[1].angle = 180
 time.sleep(1)
-kit.servo[0].angle = 0
-kit.servo[1].angle = 0
+kit.servo[0].angle = 200
+kit.servo[1].angle = 200
 time.sleep(1) 
 kit.servo[0].angle = 0
 kit.servo[1].angle = 0
