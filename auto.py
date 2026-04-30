99b90d76bbf542776297d2a4ef825fc402598833
HeimirBerg <heimir24@ru.is>
2026-04-30 13:13:10 +0000

    a

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 from skynjun import *
 def autopilot():
     while True:
-        s1, s2 = skynjun()
+        s1, s2,hf,stor = skynjun()
         print(f"Skynjari 1: {s1}, Skynjari 2: {s2}")
         if s1 and s2 == 0:
             pass #Stoppa og snúa við
