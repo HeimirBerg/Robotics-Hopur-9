2303cee44303a82db05f21c64924c5ab19432284
HeimirBerg <heimir24@ru.is>
2026-04-30 10:55:39 +0000

    main.py uppfærsla

--- skynjun.py
+++ skynjun.py
@@ -9,35 +9,34 @@ def skynjun():
     last_value1 = 800
     last_value2 = 800
 
-    i = 1
-    while i == 1:
-        i2c_bus.write_byte_data(i2c_address1, 0, 0x51)
-        i2c_bus.write_byte_data(i2c_address2, 0, 0x51)
+    
+    i2c_bus.write_byte_data(i2c_address1, 0, 0x51)
+    i2c_bus.write_byte_data(i2c_address2, 0, 0x51)
 
-        time.sleep(0.07)  # 70ms
+    time.sleep(0.07)  # 70ms
 
-        data1 = i2c_bus.read_i2c_block_data(i2c_address1, 0, 4)
-        data2 = i2c_bus.read_i2c_block_data(i2c_address2, 0, 4)
+    data1 = i2c_bus.read_i2c_block_data(i2c_address1, 0, 4)
+    data2 = i2c_bus.read_i2c_block_data(i2c_address2, 0, 4)
 
-        current_value1 = data1[2] * 256 + data1[3]
-        current_value2 = data2[2] * 256 + data2[3]
+    current_value1 = data1[2] * 256 + data1[3]
+    current_value2 = data2[2] * 256 + data2[3]
 
-        if last_value1 < 20:
+    if last_value1 < 20:
             current_value1 = 0
-        elif last_value1 > 550:
+    elif last_value1 > 550:
             current_value1 = 800
-        else:
-            pass
-        if last_value2 < 20:
-            current_value2 = 0
-        elif last_value1 > 550:
-            current_value2 = 800
-        else:
-            pass
-        last_value1 = current_value1
-        last_value2 = current_value2
+    else:
+        pass
+    if last_value2 < 20:
+        current_value2 = 0
+    elif last_value1 > 550:
+        current_value2 = 800
+    else:
+        pass
+    last_value1 = current_value1
+    last_value2 = current_value2
 
         
 
 
-        return current_value1, current_value2
+    return current_value1, current_value2
