1405f7ab5a427d4d31152374bd76b5f83e660fd6
HeimirBerg <heimir24@ru.is>
2026-04-30 10:57:58 +0000

    main

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ from skynjun import *
 from hreyfing import *
 from manual import *
 
-from skynjun import *
-from hreyfing import *
-from manual import *
-
 while True:
     adalval = int(input(" Vinsamlegast veldu eftirfarandi:\n 1 - Autopilot\n 2 - Manual Mode\n 3 - Hætta\n "))
     if adalval == 1:
-        while True:
-            s1, s2 = skynjun()
-            print(f"Skynjari 1: {s1}, Skynjari 2: {s2}")
-            if input("Ýttu á 0 til að fara aftur: ") == "0":
-                break
+        try:
+            while True:
+                s1, s2 = skynjun()
+                print(f"Skynjari 1: {s1}, Skynjari 2: {s2}")
+        except KeyboardInterrupt:
+            print("\nHætt við autopilot")
     elif adalval == 2:
         manual()
     elif adalval == 3:
