c258915c3e04b5f58939eaadfe2d59289d9e3f75
viktornoes <viktoro24@ru.is>
2026-05-01 13:01:16 +0000

    Nokkrum föllum bætt við og servoinn settur með skynjara

--- tulkari2.py
+++ tulkari2.py
@@ -1,9 +1,10 @@
 # skynjun
 
 import time
-import threading
 from smbus import SMBus
+from adafruit_servokit import ServoKit
 
+kit = ServoKit(channels=8)
 i2c_bus = SMBus(1)
 i2c_address1 = 0x71
 i2c_address2 = 0x72
@@ -20,51 +21,77 @@ def sense(last1,last2):
     current_value1 = data1[2] * 256 + data1[3] # Reiknum saman hvað kom út
     current_value2 = data2[2] * 256 + data2[3]
 
-    if current_value1 < 20:
-        current_value1 = 1
-    if current_value2 < 20:
-        current_value2 = 1
+    if current_value1 < 15:   # Skilgreinum ekkert endurkast sem 10
+        current_value1 = 10
+    if current_value2 < 15:
+        current_value2 = 10
 
-    if current_value1 < 20 and last1 < 50:         # Skynjari túlkar ekkert endurkast sem 0, skilgreinum það sem seinasta
-        current_value1 = last1
+    if current_value1 < 15 and last1 < 50:      # Eih bull sem ég skil ekki sjálfur
+        current_value1 = 10
         print("of nálægt")
-    elif current_value1 < 20 and last1 > 500:
+    elif current_value1 < 15 and last1 >= 50:
         current_value1 = last1
         print("langt")
-    if current_value2 < 20 and last2 < 50:         
-        current_value2 = last2
+    if current_value2 < 15 and last2 < 50:         
+        current_value2 = 10
         print("of nálægt")
-    elif current_value1 < 20 and last2 > 500:
+    elif current_value1 < 15 and last2 >= 50:
         current_value1 = last2
         print("langt")
-
-    if current_value1 == current_value2:
-        hlutf = 1
-        lengri = "jafnt"
-    elif current_value1 > current_value2:
-        hlutf = round(current_value1/current_value2, 4)
-        lengri = "1 er stærri"
-    elif current_value2 > current_value1:
-        hlutf = round(current_value2/current_value1, 4)
-        lengri = "2 er stærri"
     
     last1 = current_value1
     last2 = current_value2
 
-    print(f"Skynjari 1: {current_value1} cm    Skynjari 2: {current_value2} cm      Hlutfall: {hlutf} {lengri}")
-    return last1, last2, hlutf
+    print(f"Skynjari 1: {current_value1} cm    Skynjari 2: {current_value2} cm")
+    return last1, last2
 
-def hlutfall(a,b):
-    v = a/255
-    h = b/255
+def hlutfall(merki1, merki2):
+    if merki1 < 100 or merki2 < 100: # Notum fallið ef fjarlægð frá hlut er undir 100cm
+        pass
+    if merki1 == merki2: # Hér finnum við svo hlutfallið milli fjarlægðanna og hvor þeirra er lengra frá hindrun.
+        hlutf = 1
+        lengri = 0
+    elif merki1 > merki2:
+        hlutf = merki1/merki2
+        lengri = 1
+    elif merki1 < merki2:
+        hlutf = merki2/merki1
+        lengri = 2
+    return hlutf, lengri
     
+def hlidar(skyn1, skyn2): # Þetta fall er ætlað til þess að finna hvert á að beygja ef við lendum á hindrun
+    kit.servo[0].angle = 90
+    kit.servo[1].angle = 90
+    time.sleep(1)  # Gefum örmum tíma til að hreyfa sig áður en við tökum mælingu
+    x = sense(skyn1, skyn2)
+    finnatt = hlutfall(x[0], x[1])
+    uttak = 0
+    if finnatt[1] == 0:
+        uttak = 0
+    elif finnatt[1] == 1:
+        uttak =  1
+    elif finnatt[1] == 2:
+        uttak = 2
+    kit.servo[0].angle = 0
+    kit.servo[1].angle = 0
+    time.sleep(1)
+    return uttak 
+    
+def tekkv(skyn1, skyn2):
+    kit.servo[0].angle = 90
+    time.sleep(1)
+    x = sense(skyn1, skyn2)
+    plass = x[0]
+    if plass == 10:
+        pass # ??
+    elif plass >= 150:
+        pass # Beygjum í þá áttina
+
+
+
 uskyn1 = 500
 uskyn2 = 500
-
 while True:
     sjon = sense(uskyn1,uskyn2)
     uskyn1 = sjon[0]
     uskyn2 = sjon[1]
-
-
-    
