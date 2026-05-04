8f22326e609cb82d17471f2d73624b189acc32b8
viktornoes <viktoro24@ru.is>
2026-05-04 11:29:47 +0000

    update

--- auto.py
+++ auto.py
@@ -3,7 +3,7 @@ from servo import *
 def autopilot():
     while True:
         for servo0, servo1 in servo_move():
-            s0, s1 = sense()
+            s0, s1 = sense(s0, s1)
 
             print(f"Skynjari 0: {s0},  Skynjari 1: {s1}")
         
