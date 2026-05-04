9b1832dabaa7523d2a2fe4fdf4fd226cd5c2e96e
Paczio24 <patryk24@ru.is>
2026-05-04 10:50:28 +0000

    111

--- servo-PL.py
+++ servo-PL.py
@@ -13,7 +13,7 @@ def servo_move():
             kit.servo[0].angle = servo0_angle
             kit.servo[1].angle = servo1_angle
 
-            time.sleep(1)
+            time.sleep(0.03)
 
             yield servo0_angle, servo1_angle
 
@@ -25,7 +25,7 @@ def servo_move():
             kit.servo[0].angle = servo0_angle
             kit.servo[1].angle = servo1_angle
 
-            time.sleep(1)
+            time.sleep(0.03)
 
             yield servo0_angle, servo1_angle
 
