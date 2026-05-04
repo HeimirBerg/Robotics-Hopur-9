684810d228bc8958a276b6c5745fc122bf63ff92
HeimirBerg <heimir24@ru.is>
2026-05-04 13:59:03 +0000

    a

--- auto.py
+++ auto.py
@@ -25,13 +25,21 @@ def autopilot(servoenable=True):
                 fara_afram(hradi)
 
             elif s0 > 100 and s1 <= 100:
-                reikna_beygju(servo0, s1, hradi, servoenable)
+                if servoenable:
+                    reikna_beygju(servo0, s1, hradi, servoenable)
+                else:
+                    radius = int(hradi * (s1 - 20) / 80)
+                    beygja("Vinstri", hradi, max(0, radius))
 
             elif s1 > 100 and s0 <= 100:
-                reikna_beygju(servo1, s0, hradi, servoenable)
+                if servoenable:
+                    reikna_beygju(servo1, s0, hradi, servoenable)
+                else:
+                    radius = int(hradi * (s0 - 20) / 80)
+                    beygja("Hægri", hradi, max(0, radius))
 
             else:
                 beygja("Hægri", hradi, -hradi)
 
         if not servoenable:
-            time.sleep(0.1)  
+            time.sleep(0.1)
