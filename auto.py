991e396575cf2bb5590fc21b368026b4454e9ecf
HeimirBerg <heimir24@ru.is>
2026-05-04 11:44:54 +0000

    ...

--- auto.py
+++ auto.py
@@ -1,25 +1,10 @@
 from skynjun import *
 from servo import *
-def sense1():
-    while True:
-        i2c_bus.write_byte_data(i2c_address1, 0, 0x51)
-        time.sleep(0.1)
-        data1 = i2c_bus.read_i2c_block_data(i2c_address1, 0, 4)
-        
 
-        i2c_bus.write_byte_data(i2c_address2, 0, 0x51)
-        time.sleep(0.1)
-        data2 = i2c_bus.read_i2c_block_data(i2c_address2, 0, 4)
-
-        raw1 = data1[2] * 256 + data1[3]
-        raw2 = data2[2] * 256 + data2[3]
-
-        return f"Raw 0: {raw1},  Raw 1: {raw2}"
-        time.sleep(0.5)
 def autopilot():
     while True:
         for servo0, servo1 in servo_move():
-            s0, s1 = sense1()
+            s0, s1 = sense()
             
             print(f"Skynjari 0: {s0},  Skynjari 1: {s1}")
         
