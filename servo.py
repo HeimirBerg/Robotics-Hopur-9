026227d872fafc786971a2486636ead9f5e8b644
Paczio24 <patryk24@ru.is>
2026-05-04 09:43:02 +0000

    2222

--- servo.py
+++ servo.py
@@ -3,14 +3,14 @@ from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=8)
 
-kit.servo[0].angle = 40
-kit.servo[1].angle = 140
+kit.servo[0].angle = 35
+kit.servo[1].angle = 135
 time.sleep(1)
 kit.servo[0].angle = 180
 kit.servo[1].angle = 0
 time.sleep(1)
-kit.servo[0].angle = 40
-kit.servo[1].angle = 140
+kit.servo[0].angle = 35
+kit.servo[1].angle = 135
 time.sleep(1) 
 kit.servo[0].angle = 180
 kit.servo[1].angle = 0
