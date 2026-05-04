5e747dda56c28c32972c208c80113db59bf4ef59
HeimirBerg <heimir24@ru.is>
2026-05-04 11:04:43 +0000

    3

--- auto.py
+++ auto.py
@@ -3,7 +3,7 @@ from servo import *
 def autopilot():
     while True:
         servo0, servo1 = servo_move()
-        s0, s1 = skynjun()
+        #s0, s1 = skynjun()
 
         print(f"servo 0: {servo0},  Servo 1: {servo1}")
         
