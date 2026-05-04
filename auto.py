f299b7a70026334f70bdcaf8cc3e1c34120598b3
HeimirBerg <heimir24@ru.is>
2026-05-04 12:21:46 +0000

    prufa

--- auto.py
+++ auto.py
@@ -5,15 +5,27 @@ from hreyfing import *
 
 def autopilot():
     s0, s1 = 200, 200
-    hradi = 100
+    hradi = 150
     while True:
         for servo0, servo1 in servo_move():
             s0, s1, merki = sense(s0, s1)
-            dist = min(s0, s1)
 
-            if merki == 1:              # of nálægt, snúa við
+            if merki == 1:
+                # allt of nálægt og snúa á staðnum
                 beyja("Hægri", hradi, -hradi)
-            elif dist < 100:            # að nálgast hindrun, byrja að beyja
-                reikna_beyju(servo1, dist, hradi)
-            else:                       # keyra áfram
+
+            elif s0 > 100 and s1 > 100:
+                # keyra áfram
                 fara_afram(hradi)
+
+            elif s0 > 100 and s1 <= 100:
+                radius = int(hradi * (s1 - 20) / 80)
+                beyja("Hægri", hradi, max(0, radius))
+
+            elif s1 > 100 and s0 <= 100:
+                radius = int(hradi * (s0 - 20) / 80)
+                beyja("Vinstri", hradi, max(0, radius))
+
+            else:
+                # both blocked - spin on the spot
+                beyja("Hægri", hradi, -hradi)
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from manual import *
 from auto import *
 
-while True:
-    adalval = int(input(" Vinsamlegast veldu eftirfarandi:\n 1 - Autopilot\n 2 - Manual Mode\n 3 - Hætta\n "))
-    if adalval == 1:
-        autopilot()
-    elif adalval == 2:
-        manual()
-    elif adalval == 3:
-        stoppa()
-        break
-    else:
-        print("Vinsamlegast veldu gildandi tölu")
+try:
+    while True:
+        adalval = int(input(" Vinsamlegast veldu eftirfarandi:\n 1 - Autopilot\n 2 - Manual Mode\n 3 - Hætta\n "))
+        if adalval == 1:
+            autopilot()
+        elif adalval == 2:
+            manual()
+        elif adalval == 3:
+            stoppa()
+            break
+        else:
+            print("Vinsamlegast veldu gildandi tölu")
+except KeyboardInterrupt:
+    stoppa()
+    print("Ó fokk")
