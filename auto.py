2ed3385cbcc84dcffb86e4985b3858676dc7b7ea
HeimirBerg <heimir24@ru.is>
2026-05-04 10:41:42 +0000

    laga skynjun.py

--- auto.py
+++ auto.py
@@ -3,7 +3,7 @@ from servo import *
 def autopilot():
     while True:
         servo0, servo1 = servo_move()
-
         s0, s1 = skynjun()
+        
         print(f"Skynjari 1: {s1}, Skynjari 2: {s2}")
         
