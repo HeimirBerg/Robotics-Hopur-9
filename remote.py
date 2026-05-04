90f1655ebeb9886ee932d0304688ea436b212a3b
Gunnar237 <gunnarg25@ru.is>
2026-05-04 17:15:41 +0000

    sett inn keys

--- remote.py
+++ remote.py
@@ -51,24 +51,7 @@ def select_input_device(device_name: str) -> str | None:
 # if __name__ == "__main__":
 #     path = get_device_path(2)
 #     print(path)
-#     keyboard = InputDevice(path)
 
-#     for event in keyboard.read_loop():
-#         if event.type == ecodes.EV_KEY:
-#             key = categorize(event)
-            
-#             if key.keystate == key.key_down or key.keystate == key.key_hold:
-#                 if key.keycode == 'KEY_W':
-#                     print("Er hér í w")
-#                 elif key.keycode == 'KEY_S':
-#                     print("Er hér í s")
-#                 elif key.keycode == 'KEY_A':
-#                     print("Er hér í a")
-#                 elif key.keycode == 'KEY_D':
-#                     print("Er hér í d")
-                    
-#             elif key.keystate == key.key_up:
-#                 print("Fór hingað")
 
 # Test code
 if __name__ == "__main__":
@@ -81,3 +64,22 @@ if __name__ == "__main__":
         path = select_input_device(name)
 
     print(path, "was selected.\n")
+
+    keyboard = evdev.InputDevice(path)
+
+    for event in keyboard.read_loop():
+        if event.type == evdev.ecodes.EV_KEY:
+            key = evdev.categorize(event)
+            
+            if key.keystate == key.key_down or key.keystate == key.key_hold:
+                if key.keycode == 'KEY_W':
+                    print("Er hér í w")
+                elif key.keycode == 'KEY_S':
+                    print("Er hér í s")
+                elif key.keycode == 'KEY_A':
+                    print("Er hér í a")
+                elif key.keycode == 'KEY_D':
+                    print("Er hér í d")
+                    
+            elif key.keystate == key.key_up:
+                print("Fór hingað")
