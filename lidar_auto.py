f600c99725c849cf70344395b875d341f36c55a4
HeimirBerg <heimir24@ru.is>
2026-05-04 18:14:13 +0000

    a

--- lidar_auto.py
+++ lidar_auto.py
@@ -1,4 +1,4 @@
-from lidarprufa import start_lidar, stop_lidar, sense
+from lidarprufa import start_lidar, stop_lidar, sense, _min_dist_in_zone, _latest_scan, _scan_lock, FRONT_ZONE, LEFT_ZONE, RIGHT_ZONE
 from hreyfing import *
 import time
 
@@ -12,6 +12,14 @@ def autopilot():
         while True:
             try:
                 s0, s1, merki = sense(s0, s1)
+
+                with _scan_lock:
+                    scan = list(_latest_scan)
+                front = _min_dist_in_zone(scan, *FRONT_ZONE)
+                left  = _min_dist_in_zone(scan, *LEFT_ZONE)
+                right = _min_dist_in_zone(scan, *RIGHT_ZONE)
+                print(f"front={front:.0f}cm  left={left:.0f}cm  right={right:.0f}cm  s0={s0:.0f}  s1={s1:.0f}  merki={merki}")
+
             except Exception as e:
                 print(f"Sense error: {e}")
                 time.sleep(0.1)
@@ -20,21 +28,17 @@ def autopilot():
             if merki == 1:
                 beygja("Hægri", hradi, -hradi)
                 time.sleep(0.5)
-
             elif s0 > 100 and s1 > 100:
                 fara_afram(hradi)
                 time.sleep(0.05)
-
             elif s0 > 100 and s1 <= 100:
                 radius = int(hradi * (s1 - 20) / 80)
                 beygja("Vinstri", hradi, max(0, radius))
                 time.sleep(0.3)
-
             elif s1 > 100 and s0 <= 100:
                 radius = int(hradi * (s0 - 20) / 80)
                 beygja("Hægri", hradi, max(0, radius))
                 time.sleep(0.3)
-
             else:
                 beygja("Hægri", hradi, -hradi)
                 time.sleep(0.5)
