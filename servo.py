d0341fe2eb89d19df5662a8756b9f2efa798bc0f
HeimirBerg <heimir24@ru.is>
2026-05-04 11:11:58 +0000

    breyttur hraði á servo í 140ms

--- servo.py
+++ servo.py
@@ -13,7 +13,7 @@ def servo_move():
         kit.servo[0].angle = servo0_angle
         kit.servo[1].angle = servo1_angle
 
-        time.sleep(0.0025)
+        time.sleep(0.14)
 
         yield servo0_angle, servo1_angle
 
@@ -25,7 +25,7 @@ def servo_move():
         kit.servo[0].angle = servo0_angle
         kit.servo[1].angle = servo1_angle
 
-        time.sleep(0.0025)
+        time.sleep(0.14)
 
 
         yield servo0_angle, servo1_angle
