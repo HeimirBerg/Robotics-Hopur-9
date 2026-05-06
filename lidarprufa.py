baf78c8f220d420bab02c11be9853460d0b4829e
HeimirBerg <heimir24@ru.is>
2026-05-06 11:20:14 +0000

    a

--- lidarprufa.py
+++ lidarprufa.py
@@ -3,7 +3,7 @@ from pyrplidar import PyRPlidar
 lidar = PyRPlidar()
 lidar.connect(port="/dev/ttyUSB0", baudrate=1000000, timeout=3)
 lidar.set_motor_pwm(660)  # start the motor
-
+time.sleep(1)
 scan_gen = lidar.start_scan()
 
 for scan in scan_gen():
