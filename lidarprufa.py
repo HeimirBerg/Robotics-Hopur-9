b730457e8e7c4a55920e6462c89f4bf75548c846
HeimirBerg <heimir24@ru.is>
2026-05-06 11:50:44 +0000

    a

--- lidarprufa.py
+++ lidarprufa.py
@@ -3,14 +3,8 @@ import time
 
 lidar = PyRPlidar()
 lidar.connect(port="/dev/ttyUSB0", baudrate=1000000, timeout=3)
-lidar.reset()
-time.sleep(5)
-
-# flush leftover reset bytes
-for attr in ['_serial', 'serial', '_serial_port', 'serial_port', '_port', 'port']:
-    if hasattr(lidar.lidar_serial, attr):
-        getattr(lidar.lidar_serial, attr).reset_input_buffer()
-        break
+lidar.lidar_serial.set_dtr(False)  # enables motor on S2L
+time.sleep(1)
 
 scan_gen = lidar.start_scan()
 
@@ -18,5 +12,6 @@ try:
     for scan in scan_gen():
         print(scan.angle, scan.distance)
 except KeyboardInterrupt:
+    lidar.lidar_serial.set_dtr(True)
     lidar.stop()
     lidar.disconnect()
