e120f08ada0d2b7c5a1f1ca742577493ee70e066
HeimirBerg <heimir24@ru.is>
2026-05-06 11:33:43 +0000

    a

--- lidarprufa.py
+++ lidarprufa.py
@@ -1,9 +1,18 @@
 from pyrplidar import PyRPlidar
 import time
+try:
+    lidar = PyRPlidar()
+    lidar.connect(port="/dev/ttyUSB0", baudrate=1000000, timeout=3)
+    lidar.set_motor_pwm(660)  # start the motor
+    time.sleep(2)
+    print("jo")
+    scan_gen = lidar.start_scan()
 
-lidar = PyRPlidar()
-lidar.connect(port="/dev/ttyUSB0", baudrate=1000000, timeout=3)
-lidar.stop()
-lidar.set_motor_pwm(0)
-time.sleep(1)
-lidar.disconnect()
+    for scan in scan_gen():
+        print(scan.angle)      # 0-360 degrees
+        print(scan.distance)   # distance in mm (divide by 10 for cm)
+        print(scan.start_flag) # True when a new rotation begins
+except KeyboardInterrupt:
+    lidar.set_motor_pwm(0)
+    lidar.stop()
+    lidar.disconnect()
