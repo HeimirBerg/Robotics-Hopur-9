e10f3a1ea72e0fb8cca84d2629d32ab5daacba38
viktornoes <viktoro24@ru.is>
2026-05-07 15:10:51 +0000

    snua

--- kort.py
+++ kort.py
@@ -2,7 +2,21 @@
 import time
 from hreyfing import *
 
+def tiusent():
+    senda(255, -255) ## 10cm
+    time.sleep(0.25)
+    stoppa()
 
-senda(255, -255)
-time.sleep(0.25)
-stoppa()
+def snua():
+    senda(255, 255)
+    time.sleep(0.4)
+    stoppa()
+
+while True:
+    x = input("Hvað á að gera?")
+    if x == 1:
+        tiusent()
+        break
+    elif x == 2:
+        snua()
+        break
