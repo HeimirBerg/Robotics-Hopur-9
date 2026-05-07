4000a83b72cd8d3eacbf28e2e51be916db5cda33
HeimirBerg <heimir24@ru.is>
2026-05-07 12:28:18 +0000

    a

--- lidar_auto.py
+++ lidar_auto.py
@@ -18,40 +18,44 @@ def is_stuck():
         return False
     return max(recent_fronts) - min(recent_fronts) < STUCK_THRESHOLD
 
-while True:
-    front = get_distance(315, 45)
-    left  = get_distance(225, 315)
-    right = get_distance(45, 135)
+def autopilot():
+    try:
+        while True:
+            front = get_distance(315, 45)
+            left  = get_distance(225, 315)
+            right = get_distance(45, 135)
 
-    recent_fronts.append(front)
+            recent_fronts.append(front)
 
-    print(f"front: {front:.0f}  left: {left:.0f}  right: {right:.0f}")
+            print(f"front: {front:.0f}  left: {left:.0f}  right: {right:.0f}")
 
-    if is_stuck():
-        print("Stuck! Turning around...")
-        recent_fronts.clear()
-        fara_aftur(speed)
-        time.sleep(0.8)
-        beygja("Hægri", speed, -speed)
-        time.sleep(1.0)
+            if is_stuck():
+                print("Stuck! Turning around...")
+                recent_fronts.clear()
+                fara_aftur(speed)
+                time.sleep(0.8)
+                beygja("Hægri", speed, -speed)
+                time.sleep(1.0)
 
-    elif front > turn_distance:
-        fara_afram(speed)
+            elif front > turn_distance:
+                fara_afram(speed)
 
-    elif front > stop_distance:
-        inner = int(speed * front / turn_distance)
-        if left > right:
-            beygja("Vinstri", speed, inner)
-        else:
-            beygja("Hægri", speed, inner)
+            elif front > stop_distance:
+                inner = int(speed * front / turn_distance)
+                if left > right:
+                    beygja("Vinstri", speed, inner)
+                else:
+                    beygja("Hægri", speed, inner)
 
-    else:
-        stoppa()
-        time.sleep(0.2)
-        if left > right:
-            beygja("Vinstri", speed, -speed)
-        else:
-            beygja("Hægri", speed, -speed)
-        time.sleep(0.5)
+            else:
+                stoppa()
+                time.sleep(0.2)
+                if left > right:
+                    beygja("Vinstri", speed, -speed)
+                else:
+                    beygja("Hægri", speed, -speed)
+                time.sleep(0.5)
 
-    time.sleep(0.1)
+            time.sleep(0.1)
+    finally:
+        stop_lidar()
