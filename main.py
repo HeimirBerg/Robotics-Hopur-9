f5bece72227cfe90442deed45aa842e561508ca6
HeimirBerg <heimir24@ru.is>
2026-05-14 17:41:08 +0000

    a

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def lalli() -> None:
                                 stdout=subprocess.DEVNULL,
                                 stderr=subprocess.DEVNULL, 
                                 stdin=subprocess.PIPE) # Keyrir myndavél í bakgrunni
-    time.sleep(1)
+    time.sleep(3)
     while True:
         print("Hvað viltu að ég geri?")
         print("1 - Sjálfvirk keyrsla\n2 - Hanvirk keyrsla\n3 - Hætta")
