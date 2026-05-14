5321e4b74c1b557ed0207826cd27e19c0361f4af
viktornoes <viktoro24@ru.is>
2026-05-14 13:32:58 +0000

    tonlist

--- tonlist.py
+++ tonlist.py
@@ -1,22 +1,40 @@
 # Tónlist
 import random
-import os
 import subprocess
 import time
 
-def tonlist(skra): # Notum þetta fall til að spila tónlist
-    lag = subprocess.Popen(f"mpg123 -a plughw:3,0 --mono \"music/{skra}.mp3\"", shell=True)
-    return lag
+spilari = None #Hátalarinn
 
-tonar = ["guiltynigga", "baciate", "barbie", "Adolf Hitler Speech in 1935"]
+def term(skra): # Notum þetta fall til að skrifa
+    return f"mpg123 -a plughw:3,0 --mono \"music/{skra}.mp3\""
+
+def spilatonlist(skra):
+    global spilari
+    stopdamusic()
+    try:
+        spilari = subprocess.Popen(
+            term(skra),shell=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL)
+    except Exception as e:
+        print(f"Error: {e}")
+
+def stopdamusic():
+    global spilari
+    if spilari is not None:
+        spilari.terminate()
+        spilari = None
 
 def rtonn(): # Spilum lag af handahófi
-    tonlist(random.choice(tonar))
+    tonar = ["guiltynigga", "baciate", "barbie", "Adolf Hitler Speech in 1935"]
+    spilatonlist(random.choice(tonar))
+
+
 
 x = input("Veldu 1 eða 2 fyrir valið eða handahófskennt")
 if x == "1":
     skra = input("Veldu lag: ")
-    lag = tonlist(skra)
+    lag = spilatonlist(skra)
     time.sleep(5)
     lag.terminate()
 elif x == "2":
